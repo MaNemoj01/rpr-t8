54b66c51d87b73213b9a62986c4bbc50ea851917
MaNemoj01 <44163914+MaNemoj01@users.noreply.github.com>
2018-12-20 14:30:54 +0100

    prepravljen main da ljepse izgleda aplikacija

--- src/ba/unsa/etf/rpr/tutorijal8/Main.java
+++ src/ba/unsa/etf/rpr/tutorijal8/Main.java
@@ -6,13 +6,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("pretrazivac.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        FXMLLoader loader = new FXMLLoader( getClass().getResource("pretrazivac.fxml") );
+        Parent root = loader.load();
+        loader.setController( new PretrazivacController() );
+        primaryStage.setTitle("Pretraga datoteka");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         primaryStage.show();
     }
 
